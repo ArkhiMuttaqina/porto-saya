b5a17daac9b038f3e6d566e4243274d03ab62ff9
Arkhi Muttaqina <arkhi07@hotmail.co.id>
2023-08-21 14:58:53 +0700

    Push ketiga

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: true,
+    reactStrictMode: true,
     output: 'export',
+    basePath: '/github-pages',
 }
 module.exports = nextConfig
 
