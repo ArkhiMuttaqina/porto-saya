c0e6f5768868c307876fbb163effe7bfbbb55e5e
Arkhi Muttaqina <arkhi07@hotmail.co.id>
2023-08-21 14:47:32 +0700

    Push kedua

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
-// var http = require('http');
-// const express = require('express');
-// const next = require('next');
-// var server = http.createServer(function(req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     var message = 'It works!\n',
-//         version = 'NodeJS ' + process.versions.node + '\n',
-//         response = [message, version].join('\n');
-//     res.end(response);
-// });
-// server.listen();
-
-
-const http = require('http');
-const express = require('express');
-const next = require('next');
+const {
+    createServer
+} = require('http')
+const {
+    parse
+} = require('url')
+const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'localhost'
+const port = 3000
+// when using middleware `hostname` and `port` must be provided below
 const app = next({
-    dev
+    dev,
+    hostname,
+    port
 })
 const handle = app.getRequestHandler()
 
-app.prepare()
-    .then(() => {
-        const server = express()
+app.prepare().then(() => {
+    createServer(async (req, res) => {
+            try {
+                // Be sure to pass `true` as the second argument to `url.parse`.
+                // This tells it to parse the query portion of the URL.
+                const parsedUrl = parse(req.url, true)
+                const {
+                    pathname,
+                    query
+                } = parsedUrl
 
-        server.get('*', (req, res) => {
-            return handle(req, res)
+                if (pathname === '/a') {
+                    await app.render(req, res, '/a', query)
+                } else if (pathname === '/b') {
+                    await app.render(req, res, '/b', query)
+                } else {
+                    await handle(req, res, parsedUrl)
+                }
+            } catch (err) {
+                console.error('Error occurred handling', req.url, err)
+                res.statusCode = 500
+                res.end('internal server error')
+            }
         })
-
-        server.listen();
-    })
-    .catch((ex) => {
-        console.error(ex.stack)
-        process.exit(1)
-    });
-
+        .once('error', (err) => {
+            console.error(err)
+            process.exit(1)
+        })
+        .listen(port, () => {
+            console.log(`> Ready on http://${hostname}:${port}`)
+        })
+})
